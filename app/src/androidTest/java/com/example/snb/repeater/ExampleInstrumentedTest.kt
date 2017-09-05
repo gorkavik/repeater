22e96c2d1deba317c87a2bb5dc1e33ad1482cdad
Belko <siarhei.belko@instinctools.ru>
2017-09-05 15:59:36 +0300

    add kotlin support

---
+++ app/src/androidTest/java/com/example/snb/repeater/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.example.snb.repeater
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.snb.repeater", appContext.packageName)
+    }
+}
