30c3b088238123439a1f2ce689e20cfbe7f6f3c5
snb <segey.nebo@gmail.com>
2017-09-16 19:13:59 +0300

    remove fullscreenproject to empty project

--- app/src/androidTest/java/com/snb/repeater/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/snb/repeater/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.snb.repeater
+package com.snb.repeater
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -6,18 +6,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-
     @Test
     @Throws(Exception::class)
     fun useAppContext() {
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.snb.repeater", appContext.packageName)
+
+        assertEquals("com.snb.repeater", appContext.packageName)
     }
 }
--- app/src/test/java/com/snb/repeater/ExampleUnitTest.kt
+++ app/src/test/java/com/snb/repeater/ExampleUnitTest.kt
@@ -1,15 +1,9 @@
-package com.example.snb.repeater
+package com.snb.repeater
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
 class ExampleUnitTest {
-
     @Test
     @Throws(Exception::class)
     fun addition_isCorrect() {
