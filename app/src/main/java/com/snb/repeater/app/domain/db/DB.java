806d5ac1eeef7c238cc5a0ca1a9388d3a4a76b0c
GorkavikO <gorkavikolga@gmail.com>
2018-01-29 21:24:53 +0300

    TASK

---
+++ app/src/main/java/com/snb/repeater/app/domain/db/DB.java
@@ -0,0 +1,11 @@
+package com.snb.repeater.app.domain;
+
+public class DB {
+//    Assigment:
+//    Read: https://habrahabr.ru/post/336196/
+//    Understand: what is Room? and what for it?
+//    Create entity, dao for it.
+//    Implement CRUD methods (create, read, update, delete)
+//    *** Add tests for each method
+//    info
+}
