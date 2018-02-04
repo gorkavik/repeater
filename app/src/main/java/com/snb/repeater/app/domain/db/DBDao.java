37ef62d04754d9391f6d627d586c0b72aad7919a
GorkavikO <gorkavikolga@gmail.com>
2018-02-04 20:53:12 +0300

    add room, entity, dao

---
+++ app/src/main/java/com/snb/repeater/app/domain/db/DBDao.java
@@ -0,0 +1,8 @@
+package com.snb.repeater.app.domain.db;
+
+/**
+ * Created by Admin on 04.02.2018.
+ */
+
+public interface DBDao {
+}
---
+++ app/src/main/java/com/snb/repeater/app/domain/db/DBHelper.java
@@ -0,0 +1,10 @@
+package com.snb.repeater.app.domain.db;
+
+/**
+ * Created by
+ * Gorkavik_O
+ * 04.02.2018.
+ */
+
+public abstract class DBHelper {
+}
