8fbcd558ef945d93a250d64c4704de7bcafe9478
snb <segey.nebo@gmail.com>
2018-07-02 18:27:02 +0300

    refactor code

--- app/src/main/java/com/snb/repeater/app/ui/activity/StartActivity.java
+++ app/src/main/java/com/snb/repeater/app/ui/activity/StartActivity.java
@@ -1,8 +1,7 @@
 package com.snb.repeater.app.ui.activity;
 
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.snb.repeater.R;
 
@@ -11,11 +10,6 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
-
-        //ImageView imagelogo = (ImageView) findViewById(R.id.imagelogo);
-        //imagelogo.setImageDrawable(Drawable.createFromPath());
-
     }
 }
